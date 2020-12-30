935e444901a85b8751164e44a128f96b704f1332
Slava Maksymchuk <mail4maxsl@gmail.com>
2020-12-30 13:02:52 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:cubrider/screens/mainpage.dart';
+import 'package:cubrider/screens/loginpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: 'db2',
+    options: Platform.isIOS || Platform.isMacOS
+        ? FirebaseOptions(
+      appId: '1:297855924061:ios:c6de2b69b03a5be8',
+      apiKey: 'AIzaSyD_shO5mfO9lhy2TVWhfo1VUmARKlG4suk',
+      projectId: 'flutter-firebase-plugins',
+      messagingSenderId: '297855924061',
+      databaseURL: 'https://beetaxi-f0f10-default-rtdb.europe-west1.firebasedatabase.app',
+    )
+        : FirebaseOptions(
+      appId: '1:490594976115:android:ec98282689c890a41f4506',
+      apiKey: 'AIzaSyA8C-qjocIZB26Bq2YgfZJmY-gBxZyjQVo',
+      messagingSenderId: '490594976115',
+      projectId: 'beetaxi-f0f10',
+      databaseURL: 'https://beetaxi-f0f10-default-rtdb.europe-west1.firebasedatabase.app',
+    ),
+  );
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Brand-Regular',
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+
---
+++ lib/screens/loginpage.dart
@@ -0,0 +1,102 @@
+import 'package:cubrider/brand_colors.dart';
+import 'package:flutter/material.dart';
+ 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 70,),
+                Image(
+                  alignment: Alignment.center,
+                  height: 100.0,
+                  width: 100.0,
+                  image: AssetImage('images/logo.png'),
+                ),
+                SizedBox(height: 40),
+                Text('Sign In as a Rider',
+                textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, fontFamily: 'Brand-Bold')
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: "Email address",
+                            labelStyle: TextStyle(
+                                fontSize: 14
+                            ),
+                            hintStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0
+                            )
+                        ),
+                        style: TextStyle(fontSize: 14),
+                      ),
+
+                      SizedBox(height: 10),
+
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                                fontSize: 14
+                            ),
+                            hintStyle: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 10.0
+                            )
+                        ),
+                        style: TextStyle(fontSize: 14),
+                      ),
+
+                      SizedBox( height: 40),
+
+                      RaisedButton(
+                        onPressed: (){
+
+                        },
+                        color: BrandColors.colorGreen,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(25)
+                        ),
+                        textColor: Colors.white,
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                                'LOGIN',
+                                style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold')
+                            ),
+                          ),
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+
+                FlatButton(
+                    onPressed: (){},
+                    child: Text('Не зарегистрированы? Создайте аккаунт'))
+
+
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
